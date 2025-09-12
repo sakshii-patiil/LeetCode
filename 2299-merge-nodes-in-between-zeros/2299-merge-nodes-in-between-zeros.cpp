bc46ac0568c09ff70ead94494b6840849bebe69c
Sakshi Patil <67887279+sakshii-patiil@users.noreply.github.com>
2025-09-12 07:46:09 -0700

    Time: 103 ms (5.6%), Space: 314.4 MB (5.18%) - LeetHub

---
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        ListNode* temp = head->next;
+        ListNode* newHead =  NULL;
+        ListNode* tempNewList = new ListNode;
+        ListNode* tempNewNode = new ListNode ;
+        int sum=0;
+
+        while(temp!=NULL && temp->next!=NULL)
+        {
+            ListNode* newNode = new ListNode ;
+            sum +=temp->val;
+            temp=temp->next;
+            if(temp->val ==0)
+            {
+                newNode->val = sum;
+                newNode->next = NULL;
+                if(newHead == NULL)
+                {
+                    newHead = newNode;
+                    tempNewList = newNode;
+                }else{
+                    tempNewList->next = newNode;
+                    tempNewList=tempNewList->next;
+                }
+                sum=0;
+                temp=temp->next;
+            }
+            
+        }
+        // tempNewNode->val = sum;
+        // tempNewNode->next = NULL;
+        // tempNewList->next=tempNewNode;
+        return newHead;
+    }
+};
